d2da6d739437bd3c46d0bfc8e0ee9c70ef780319
Ronhawes <ronhawes09@gmail.com>
2025-09-11 23:42:46 +0300

    stk push

--- app/api/stkpush/page.js
+++ app/api/stkpush/page.js
@@ -149,7 +149,7 @@ export async function PUT(req) {
         { status: 200 }
       );
     } else {
-      console.error("Payment unsuccessful:", ResultDesc);
+      console.error("Payment not successful:", ResultDesc);
       return new Response(
         JSON.stringify({
           success: false,
