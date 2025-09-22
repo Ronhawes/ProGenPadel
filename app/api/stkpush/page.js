ddcd76f4988b928a8a8b30c5229ee90d0a779297
Ronhawes <ronhawes09@gmail.com>
2025-09-22 23:57:53 +0300

    stk push

--- app/api/stkpush/page.js
+++ app/api/stkpush/page.js
@@ -163,7 +163,7 @@ export async function PUT(req) {
     return new Response(
       JSON.stringify({
         success: false,
-        message: "Server error handling callback errrors ",
+        message: "Server error handling callback  ",
       }),
       { status: 500 }
     );
